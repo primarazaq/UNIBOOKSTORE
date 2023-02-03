84ff7c04eca7c865bf52863a8267638e581a12b6
primarazaq <85297003+primarazaq@users.noreply.github.com>
2023-02-04 02:03:02 +0700

    db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -52,5 +52,61 @@ class DatabaseSeeder extends Seeder
             'stok' => '60',
             'id_penerbit' => 'SP01'
         ]);
+        Buku::create([
+            'id_buku' => 'K1002',
+            'kategori' => 'Keilmuan',
+            'nama_buku' => 'Artificial Intelligence',
+            'harga' => '45000',
+            'stok' => '60',
+            'id_penerbit' => 'SP01'
+        ]);
+        Buku::create([
+            'id_buku' => 'K1003',
+            'kategori' => 'Keilmuan',
+            'nama_buku' => 'Autocad 3 Dimensi',
+            'harga' => '40000',
+            'stok' => '24',
+            'id_penerbit' => 'SP01'
+        ]);
+        Buku::create([
+            'id_buku' => 'B1001',
+            'kategori' => 'Bisnis',
+            'nama_buku' => 'Bisnis Online',
+            'harga' => '75000',
+            'stok' => '9',
+            'id_penerbit' => 'SP01'
+        ]);
+        Buku::create([
+            'id_buku' => 'K3004',
+            'kategori' => 'Keilmuan',
+            'nama_buku' => 'Cloud Computing Technology',
+            'harga' => '85000',
+            'stok' => '15',
+            'id_penerbit' => 'SP01'
+        ]);
+        Buku::create([
+            'id_buku' => 'B1002',
+            'kategori' => 'Bisnis',
+            'nama_buku' => 'Etika Bisnis dan Tanggung Jawab Sosial',
+            'harga' => '67500',
+            'stok' => '20',
+            'id_penerbit' => 'SP01'
+        ]);
+        Buku::create([
+            'id_buku' => 'N1001',
+            'kategori' => 'Novel',
+            'nama_buku' => 'Cahaya Di Penjuru Hati',
+            'harga' => '68000',
+            'stok' => '10',
+            'id_penerbit' => 'SP02'
+        ]);
+        Buku::create([
+            'id_buku' => 'N1002',
+            'kategori' => 'Novel',
+            'nama_buku' => 'Aku Ingin Cerita',
+            'harga' => '48000',
+            'stok' => '12',
+            'id_penerbit' => 'SP03'
+        ]);
     }
 }
