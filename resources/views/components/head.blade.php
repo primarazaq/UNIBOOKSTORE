9235c145651e8eb682594c73a955f64f3cc7014c
primarazaq <85297003+primarazaq@users.noreply.github.com>
2023-02-18 15:52:46 +0700

    install PWA feature

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>UNIBOOKSTORE</title>
+    @laravelPWA
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
